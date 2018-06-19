34a2419b99fbfd28627612f927362f3fec217ad6
SaitouRedEyes <zeca.saitou@gmail.com>
2018-06-19 15:10:20 -0300

    Xamarin Intents Example
    Some Intents example of use.

---
+++ IntentsSample/MainActivity.cs
@@ -0,0 +1,91 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+using Android.Content;
+
+namespace IntentsSample
+{
+    [Activity(Label = "IntentsSample", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            Button buttonOpenBrowser = FindViewById<Button>(Resource.Id.button_open_browser);
+            Button buttonCall = FindViewById<Button>(Resource.Id.button_call);
+            Button buttonMapsStreet = FindViewById<Button>(Resource.Id.button_maps_street);
+            Button buttonMapsCoordinates = FindViewById<Button>(Resource.Id.button_maps_coordinates);
+            Button buttonMapsRoutes = FindViewById<Button>(Resource.Id.button_maps_routes);
+            Button buttonContacts = FindViewById<Button>(Resource.Id.button_contacts);
+            Button buttonAudio = FindViewById<Button>(Resource.Id.button_audio);
+
+            buttonOpenBrowser.Click += OnButtonOpenBrowserClicked;
+            buttonCall.Click += OnButtonCallClicked;
+            buttonMapsStreet.Click += OnButtonMapsStreetClicked;
+            buttonMapsCoordinates.Click += OnButtonMapsCoordinatesClicked;
+            buttonMapsRoutes.Click += OnButtonMapsRoutesClicked;
+            buttonContacts.Click += OnButtonContactsClicked;
+            buttonAudio.Click += OnButtonAudioClicked;
+        }
+
+        private void OnButtonOpenBrowserClicked(object sender, EventArgs e)
+        {
+            Intent i = new Intent(Intent.ActionView, 
+                                  Android.Net.Uri.Parse("http://www.facebook.com"));
+
+            StartActivity(i);
+        }
+
+        private void OnButtonCallClicked(object sender, EventArgs e)
+        {
+            Intent i = new Intent(Intent.ActionCall, 
+                                  Android.Net.Uri.Parse("tel:34781265"));
+            StartActivity(i);
+        }
+
+        private void OnButtonMapsStreetClicked(object sender, EventArgs e)
+        {
+            Intent i = new Intent(Intent.ActionView, 
+                                  Android.Net.Uri.Parse("geo:0,0?q=Rua+Uruguai,Rio+de+Janeiro"));
+            StartActivity(i); //Precisa ter o google maps instalado no celular
+        }
+
+        private void OnButtonMapsCoordinatesClicked(object sender, EventArgs e)
+        {
+            Intent i = new Intent(Intent.ActionView, 
+                                  Android.Net.Uri.Parse("geo:-22.911667, -43.230278"));
+            StartActivity(i); //Precisa ter o google maps instalado no celular
+        }
+
+        private void OnButtonMapsRoutesClicked(object sender, EventArgs e)
+        {
+            string source = "-22.911667, -43.230278";
+            string destination = "-25.443195, -49.280977";
+            string url = "https://www.google.com.br/maps/dir/" + source + "/" + destination + "/";
+
+            Intent i = new Intent(Intent.ActionView, Android.Net.Uri.Parse(url));
+            StartActivity(i);
+        }
+
+        private void OnButtonContactsClicked(object sender, EventArgs e)
+        {
+            Intent i = new Intent(Intent.ActionPick, 
+                                  Android.Net.Uri.Parse("content://com.android.contacts/contacts/"));
+            StartActivity(i);
+        }
+
+        private void OnButtonAudioClicked(object sender, EventArgs e)
+        {
+            Intent i = new Intent(Intent.ActionView, 
+                Android.Net.Uri.Parse("http://www.servidor.com.br/musica.mp3"));
+            Intent.SetType("audio/a*");
+            StartActivity(i);
+        }
+    }
+}
+
---
+++ IntentsSample/obj/Debug/android/src/md575795b1d2cfb5de30d10e8a7a1eb0493/MainActivity.java
@@ -0,0 +1,47 @@
+package md575795b1d2cfb5de30d10e8a7a1eb0493;
+
+
+public class MainActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("IntentsSample.MainActivity, IntentsSample, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("IntentsSample.MainActivity, IntentsSample, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
